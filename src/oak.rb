2c37b27e7354f6168343cd7bbafa4b5e19cff7c6
Tim Smith <timsmith3@gmail.com>
2019-08-08 08:38:54 -0700

    split out oak.rb from proof.rb

---
+++ src/oak.rb
@@ -0,0 +1,28 @@
+if RUBY_VERSION < '2'
+	puts "Oak requires Ruby version >= 2.0.  (You have Ruby #{RUBY_VERSION}.)"
+	exit
+end
+
+require_relative 'proof.rb'
+
+name_version = 'Oak version 0.2pre'
+issues_url = 'https://github.com/smithtim/oak/issues'
+
+if ARGV.delete '-v'
+	puts name_version
+	exit if ARGV.size == 0
+end
+
+if ARGV.size != 1 or ARGV[0].start_with? '-'
+	puts "usage: oak [-v] [filename]"
+	exit
+end
+
+begin
+	Proof.new.process ARGV[0], :is_filename
+rescue => e
+	exit if e.is_a? ProofException # already printed
+	puts "\n\n#{e.message} (#{e.class}) [#{name_version}]"
+  puts "\tfrom #{e.backtrace.join "\n\tfrom "}"
+	puts "\nBUG: You have found a bug in the proof checker!  It would be greatly appreciated if you could report it at #{issues_url} so that it can be fixed."
+end
--- src/proof.rb
+++ src/proof.rb
@@ -1,8 +1,3 @@
-if RUBY_VERSION < '2'
-	puts "Oak requires Ruby version >= 2.0.  (You have Ruby #{RUBY_VERSION}.)"
-	exit
-end
-
 require_relative 'parser.rb'
 require_relative 'external prover.rb'
 require_relative 'schema.rb'
@@ -775,29 +770,3 @@ class ParseException < ProofException
 end
 
 class SubstituteException < ProofException; end
-
-##########################################################################
-
-if $0 == __FILE__
-	name_version = 'Oak version 0.2pre'
-	issues_url = 'https://github.com/smithtim/oak/issues'
-
-	if ARGV.delete '-v'
-		puts name_version
-		exit if ARGV.size == 0
-	end
-
-	if ARGV.size != 1 or ARGV[0].start_with? '-'
-		puts "usage: oak [-v] [filename]"
-		exit
-	end
-
-	begin
-		Proof.new.process ARGV[0], :is_filename
-	rescue => e
-		exit if e.is_a? ProofException # already printed
-		puts "\n\n#{e.message} (#{e.class}) [#{name_version}]"
-	  puts "\tfrom #{e.backtrace.join "\n\tfrom "}"
-		puts "\nBUG: You have found a bug in the proof checker!  It would be greatly appreciated if you could report it at #{issues_url} so that it can be fixed."
-	end
-end
